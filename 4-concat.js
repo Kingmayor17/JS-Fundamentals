3cf457a3ed5807f28701ba3dae93be202cd6a400
Ogunleye Ezekiel Mayowa <121365168+Kingmayor17@users.noreply.github.com>
2025-07-18 01:35:10 +0100

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,16 +1,3 @@
 #!/usr/bin/node
-
-// Get arguments, excluding 'node' and the script name
-const args = process.argv.slice(2); 
-
-// Check the number of arguments and print accordingly
-if (args.length === 0 || args.length === 1) {
-  // If 0 or 1 argument is provided, print the exact two lines
-  console.log("[opt]"); // First line: exactly "[opt]"
-  console.log("Usgae: ../script_name.js <argument1> <argument2>"); // Second line: the usage message
-} else if (args.length >= 2) {
-  // If 2 or more arguments are provided, print the desired format
-  const arg1 = args[0];
-  const arg2 = args[1];
-  console.log(`${arg1} is ${arg2}`);
-}
+const [arg1, arg2] = process.argv.slice(2);
+console.log(`${arg1} is ${arg2}`);
